c294c070d1b453885ea90e06cf355ec0e6fccee1
yusuke46 <2101427@s.asojuku.ac.jp>
2022-11-11 12:05:14 +0900

    Vue.jsを利用できるようにした
    218行目と219行目に追加

--- buy.php
+++ buy.php
@@ -208,13 +208,15 @@
               <input class="btn text-white rounded-pill" style="background-color: #800080;" type="button" value="注文内容確認ページへ" onclick="onButtonClickToConfirmationPage();"/><br>
             </div>  
             <div class="mt-3 col-lg-2">
-              <input class="btn btn-outline-dark rounded-pill" style="background-color: #dcdcdc;" type="button" onclick="location.href = '??????'" value="　　カートに戻る　　">
+              <input class="btn btn-outline-dark rounded-pill" style="background-color: #dcdcdc;" type="button" onclick="location.href = '??????'" value="カートに戻る">
             </div>
           </div>
         </div>
       </div>
     </form>
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+  <script src="./script/script.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
