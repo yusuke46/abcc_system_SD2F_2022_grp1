3db66c704ded1d1210128764101c54b4949daeb9
NKYkuukou <2101486@s.asojuku.ac.jp>
2022-12-14 11:38:02 +0900

    buy_Complete.phpのボタン
    ボタンをbootstrap製に変えました。

--- buy_Complete.php
+++ buy_Complete.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
-         body { 
-        padding-top: 130px;
+        body { 
+          padding-top: 130px;
+        }
+        .vacuum{
+          margin-bottom:10%;
         }
         </style>
 </head>
@@ -21,10 +24,10 @@ if(isset($_SESSION['user_id'])){
  ?>
   <!--この下から書き加える-->
   <h2 class="mt-5 mb-5" style="text-align:center">購入完了画面</h2>
-  <h3 style="text-align:center">購入しました</h3>
+  <h3 class="vacuum" style="text-align:center">購入しました</h3>
   <div style="text-align:center">
-  <input class="btn btn-lg btn-outline-dark rounded-pill" style="background-color: #ff00ff;" type="button" id="BackToCartButton" onclick="location.href = 'top.php'" value="ホームに戻る">
-    </div>
+    <input class="btn text-white rounded-pill btn-lg" style="background-color: #800080;" type="submit" value="ホームに戻る" onclick="location.href='top.php'">
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
